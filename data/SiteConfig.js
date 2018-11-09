8e042b29d89847b7947f10daf5bac3db335703fd
nxnfufunezn <nxnfufunezn@gmail.com>
2018-11-10 01:52:57 +0530

    Remove logo and desc for now

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,15 +4,14 @@ module.exports = {
   blogAuthorId: "akash", // The default and fallback author ID used for blog posts without a defined author.
   siteTitle: "Akash Sharma", // Site title.
   siteTitleAlt: "Akash Sharma", // Alternative site title for SEO.
-  siteLogo:
-    "https://haysclark.github.io/gatsby-starter-casper/logos/logo-1024.png", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
+  siteLogo: "", // Logo used for SEO and manifest. e.g. "/logos/logo-1024.png",
   siteUrl: "http://www.akashsharma.xyz", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-casper/.
   siteDescription:
-    "A Casper themed GatsbyJS stater based on the Advanced Starter.", // Website description used for RSS feeds/meta description tag.
+    "", // Website description used for RSS feeds/meta description tag.
   siteCover:
     "https://haysclark.github.io/gatsby-starter-casper/images/blog-cover.jpg", // Optional, the cover image used in header for home page. e.g: "/images/blog-cover.jpg",
-  siteNavigation: true, // If navigation is enabled the Menu button will be visible
+  siteNavigation: false, // If navigation is enabled the Menu button will be visible
   siteRss: "/rss.xml", // Path to the RSS file.
   siteRssAuthor: "Akash Sharma", // The author name used in the RSS file
   // siteFBAppID: "1825356251115265", // optional, sets the FB Application ID for using app insights
@@ -21,6 +20,7 @@ module.exports = {
   // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // enables Disqus comments, visually deviates from original Casper theme.
   siteSocialUrls: [
     "https://github.com/nxnfufunezn",
+    "https://www.linkedin.com/in/akash-sharma-x",
     "https://twitter.com/nxnfufunezn",
     "mailto:hello@akashsharma.xyz"
   ],
@@ -31,6 +31,11 @@ module.exports = {
       label: "GitHub",
       url: "https://github.com/nxnfufunezn",
       iconClassName: "fa fa-github" // Disabled, see Navigation.jsx
+    },
+    {
+      label: "LinkedIn",
+      url: "https://www.linkedin.com/in/akash-sharma-x/",
+      iconClassName: "fa fa-linkedin"
     },
     {
       label: "Twitter",
@@ -50,6 +55,6 @@ module.exports = {
     // url: "https://www.gatsbyjs.org/" // optional, set link address of copyright, defaults to site root
   },
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
-  backgroundColor: "#e0e0e0", // Used for setting manifest background color.
+  backgroundColor: "#000000", // Used for setting manifest background color.
   promoteGatsby: false // Enables the GatsbyJS promotion information in footer.
 };
