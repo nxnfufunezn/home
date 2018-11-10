6ae96e7acb546844262774afa0d2d126969b25f2
nxnfufunezn <nxnfufunezn@gmail.com>
2018-11-10 15:56:17 +0530

    Update GA tracking Id

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -16,7 +16,7 @@ module.exports = {
   siteRssAuthor: "Akash Sharma", // The author name used in the RSS file
   // siteFBAppID: "1825356251115265", // optional, sets the FB Application ID for using app insights
   sitePaginationLimit: 10, // The max number of posts per page.
-  googleAnalyticsID: "UA-111982167-1", // GA tracking ID.
+  googleAnalyticsID: "UA-129024338-1", // GA tracking ID.
   // disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // enables Disqus comments, visually deviates from original Casper theme.
   siteSocialUrls: [
     "https://github.com/nxnfufunezn",
